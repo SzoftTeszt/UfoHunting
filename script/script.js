0310fcb53d786cb7066ebb22fa8c505bb92d8cbf
Erős István <eros@valahol>
2026-01-15 19:27:50 +0100

    Kesz

--- script/script.js
+++ script/script.js
@@ -1,5 +1,11 @@
 console.log("Helló")
-
+let talalat=0
+const baseAudio = new Audio("./img/robbanas.wav")
+function boom(){
+    const a= baseAudio.cloneNode()
+    a.volume=.1
+    a.play()
+}
 
 function ufoKeszites(){
 const ufo = document.createElement("img")
@@ -12,21 +18,36 @@ const maxTop = window.innerHeight- 300;
 ufo.style.top=Math.random()*maxTop+"px"
 ufo.style.left="0px"
 ufo.addEventListener("click", robban)
+ufo.addEventListener("mouseleave", szerelmes )
+ufo.addEventListener("mouseenter", sir )
 }
 
-function mozgat(){
+function sir(){
+    this.src="./img/UFO-cry.gif"
+}
+
+function szerelmes(e){
+     e.currentTarget.src="./img/UFO-kissy.gif"
+}
+
+function mozgat(e){
    document.querySelectorAll("img").forEach(
     (ufo)=>{
         const left = parseInt(ufo.style.left)
-        console.log("ufo:", ufo)
-        console.log("left:", left)
+        // console.log("ufo:", ufo)
+        // console.log("left:", left)
         ufo.style.left=left+10+"px"
         
         if (parseInt(ufo.style.left)>window.innerWidth)
         {
             ufo.remove()
         }
-        document.querySelector("h1").innerHTML=document.querySelectorAll("img").length
+        // console.log("(parseInt(ufo.style.left)",parseInt(ufo.style.left))
+        if (parseInt(ufo.style.left)>(window.innerWidth*2/3))
+        {
+             ufo.src="./img/UFO-sleep.gif"
+        }
+        document.querySelector("h1").innerHTML=talalat
     }
    ) 
 }
@@ -39,7 +60,9 @@ function robban(event){
     robbanas.style.width="300px"
     robbanas.src="./img/bang.gif"
     ufo.replaceWith(robbanas)
+    boom()
 
+    document.querySelector("h1").innerHTML=talalat++
     setTimeout(
         ()=> robbanas.remove(), 750
     )
